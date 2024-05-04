c296f170e99c805b28ef48bd87a6109e437593fb
Sean Norwood <norwood.sean@gmail.com>
2024-05-04 09:49:03 -0500

    Clean up navbar
    Signed-off-by: Sean Norwood <norwood.sean@gmail.com>

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,35 +1,21 @@
-import {
-  SignInButton,
-  SignOutButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/remix";
+import { SignInButton, SignUpButton, SignedIn, SignedOut, UserButton } from "@clerk/remix";
 import { Link } from "@remix-run/react";
 import { Button } from "./ui/button";
 
 export function Navbar() {
   return (
-    <header className="flex items-center justify-between bg-gray-900 px-4 py-3 text-gray-50 dark:bg-gray-950 dark:text-gray-50">
+    <header className="flex items-center justify-between bg-stone-100 px-4 py-3">
       <Link className="flex items-center gap-2" to="#">
         <span className="text-lg font-semibold">Weed Tracker</span>
       </Link>
       <div className="flex items-center gap-4">
         <SignedIn>
-          <p>You are signed in!</p>
-          <div>
+          <div className="flex items-center gap-4">
             <p>View your profile here 👇</p>
             <UserButton />
           </div>
-          <div>
-            <SignOutButton>
-              <Button>Sign out</Button>
-            </SignOutButton>
-          </div>
         </SignedIn>
         <SignedOut>
-          <p>You are signed out</p>
           <div>
             <SignInButton>
               <Button>Sign In</Button>
